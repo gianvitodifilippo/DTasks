5b890c51f34ab2822d2f6d9ee7338a945f07104a
Gianvito Difilippo <65296492+GianvitoDifilippo@users.noreply.github.com>
2024-09-07 20:13:28 +0200

    Add support for redis storage (#4)
    * Add support for redis storage
    
    * Fix formatting

---
+++ src/DTasks.Storage.StackExchangeRedis/RedisDTaskStorage.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+
+namespace DTasks.Storage.StackExchangeRedis;
+
+public sealed class RedisDTaskStorage(IDatabase database) : IDTaskStorage<RedisFlowStack>
+{
+    public RedisFlowStack CreateStack()
+    {
+        return new RedisFlowStack([]);
+    }
+
+    public async Task<RedisFlowStack> LoadStackAsync<TFlowId>(TFlowId flowId, CancellationToken cancellationToken)
+      where TFlowId : notnull
+    {
+        HashEntry[] entries = await database.HashGetAllAsync(new RedisKey(flowId.ToString()));
+        return new RedisFlowStack(new Stack<HashEntry>(entries));
+    }
+
+    public Task SaveStackAsync<TFlowId>(TFlowId flowId, ref RedisFlowStack stack, CancellationToken cancellationToken)
+      where TFlowId : notnull
+    {
+        return database.HashSetAsync(flowId.ToString(), stack.GetEntries());
+    }
+}
